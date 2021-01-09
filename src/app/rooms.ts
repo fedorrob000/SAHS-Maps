afa927f9c0dc51150b4375178d82513360a9446c
joeyf123 <61664460+joeyf123@users.noreply.github.com>
2021-01-09 12:39:13 -0600

    Added Hallways and rooms

--- src/app/rooms.ts
+++ src/app/rooms.ts
@@ -1,9 +1,12 @@
 import { Room } from './room';
 
 export const ROOMS: Room[] = [
-  {roomNumber: 'D216', xPosition: 480, yPosition: 380},
   {roomNumber: 'C210', xPosition: 450, yPosition: 140},
   {roomNumber: 'C209', xPosition: 570, yPosition: 180},
-  {roomNumber: 'a1', xPosition: 20, yPosition: 40},
-  {roomNumber: 'a1', xPosition: 20, yPosition: 40}
+  {roomNumber: 'C222', xPosition: 825, yPosition: 230},
+
+  {roomNumber: 'H212', xPosition: 560, yPosition: 110},
+  {roomNumber: 'H210', xPosition: 525, yPosition: 140},
+  {roomNumber: 'H208', xPosition: 400, yPosition: 230},
+  {roomNumber: 'H222', xPosition: 770, yPosition: 210},
 ];
