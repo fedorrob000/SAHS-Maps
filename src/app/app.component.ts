27a0a242f2062eaf29b67f65b822af3012740d46
fedorrob000 <robert.fedor@ndus.edu>
2021-01-09 12:31:45 -0600

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import {ROOMS} from './rooms';
 })
 export class AppComponent implements OnInit{
   title = 'SAHS Maps';
+  private scale = 1;
 
   @ViewChild('canvas', {static: true})
   canvas: ElementRef<HTMLCanvasElement>;
@@ -27,18 +28,26 @@ export class AppComponent implements OnInit{
     const img = new Image();
     img.src = 'assets/SAHS_MAP_UP.png';
     img.onload = () => {
-      this.ctx.scale(.5,.5);
+      this.ctx.scale(this.scale, this.scale);
       this.ctx.drawImage(img,0,0);
+      this.ctx.strokeStyle = '#FF0000';
+      this.ctx.lineWidth +=3;
+      this.connectRooms(ROOMS[1], ROOMS[2]);
     };
-    this.ctx.canvas.width = window.innerWidth - 100;
-    this.ctx.canvas.height = window.innerHeight - 100;
-
+    this.ctx.canvas.width = 1400;
+    this.ctx.canvas.height = 1500;
     this.ctx.drawImage(img, 10, 10);
 
-    this.graph.AddVertex(1,ROOMS[0]);
-    this.graph.AddVertex(2,ROOMS[1]);
+    this.graph.AddVertex(1,ROOMS[1]);
+    this.graph.AddVertex(2,ROOMS[2]);
     this.graph.AddEdge(1, 2, 1);
 
+  }
+
+  connectRooms(room1: Room, room2: Room): void {
+    this.ctx.moveTo(room1.xPosition * this.scale, room1.yPosition * this.scale);
+    this.ctx.lineTo(room2.xPosition * this.scale, room2.yPosition * this.scale);
+    this.ctx.stroke();
   }
 
 }
--- src/app/rooms.ts
+++ src/app/rooms.ts
@@ -1,9 +1,9 @@
 import { Room } from './room';
 
 export const ROOMS: Room[] = [
-  {roomNumber: 'a1', xPosition: 20, yPosition: 40},
-  {roomNumber: 'a1', xPosition: 20, yPosition: 40},
-  {roomNumber: 'a1', xPosition: 20, yPosition: 40},
+  {roomNumber: 'D216', xPosition: 480, yPosition: 380},
+  {roomNumber: 'C210', xPosition: 450, yPosition: 140},
+  {roomNumber: 'C209', xPosition: 570, yPosition: 180},
   {roomNumber: 'a1', xPosition: 20, yPosition: 40},
   {roomNumber: 'a1', xPosition: 20, yPosition: 40}
 ];
