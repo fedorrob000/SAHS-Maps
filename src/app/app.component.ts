26ed597d7072f4c5eddb791408cedaba2551efc4
fedorrob000 <robert.fedor@ndus.edu>
2021-01-13 13:52:26 -0600

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,15 +30,19 @@ export class AppComponent implements OnInit {
     img.onload = () => {
       this.ctx.scale(this.scale, this.scale);
       this.ctx.drawImage(img, 0, 0);
-      this.ctx.strokeStyle = '#FF0000';
+      this.ctx.strokeStyle = '#ff0000';
       this.ctx.lineWidth += 3;
 
 
       ROOMS.forEach(room => this.graph.AddVertex(this.graph.Vertexs().length + 1, room));
       console.log(this.roomToID('H212'));
       this.graph.AddEdge(this.roomToID('H212'), this.roomToID('H210'), 1);
+      this.graph.AddEdge(this.roomToID('C210'), this.roomToID('H210'), 1);
+      this.graph.AddEdge(this.roomToID('H208'), this.roomToID('H210'), 1);
 
       this.connectRooms(this.graph.Edges()[0]);
+      this.connectRooms(this.graph.Edges()[1]);
+      this.connectRooms(this.graph.Edges()[2]);
     };
 
     this.ctx.canvas.width = 1400;
--- src/app/rooms.ts
+++ src/app/rooms.ts
@@ -1,13 +1,14 @@
 import { Room } from './room';
 
 export const ROOMS: Room[] = [
-  {roomNumber: 'C210', xPosition: 450, yPosition: 140},
+  // y is off by 10
+  {roomNumber: 'C210', xPosition: 465, yPosition: 106},
   {roomNumber: 'C209', xPosition: 570, yPosition: 180},
   {roomNumber: 'C222', xPosition: 825, yPosition: 230},
 
-  {roomNumber: 'H212', xPosition: 560, yPosition: 110},
-  {roomNumber: 'H210', xPosition: 525, yPosition: 140},
-  {roomNumber: 'H208', xPosition: 400, yPosition: 230},
+  {roomNumber: 'H212', xPosition: 555, yPosition: 102},
+  {roomNumber: 'H210', xPosition: 520, yPosition: 130},
+  {roomNumber: 'H208', xPosition: 390, yPosition: 218},
   {roomNumber: 'H222', xPosition: 770, yPosition: 210},
 
   {roomNumber: '0', xPosition: 520, yPosition: 25},
