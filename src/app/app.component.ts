04a5b7ab23db4f3ce32e7e5e2b2c2b8c9dacdcd1
fedorrob000 <robert.fedor@ndus.edu>
2021-01-16 14:36:47 -0600

    Added change maps button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,6 @@ import {ShortestPath} from "./canvas/ShortestPath";
 })
 export class AppComponent implements OnInit {
   title = 'SAHS Maps';
-  private scale = 1;
 
   @ViewChild('canvas', {static: true})
   canvas: ElementRef<HTMLCanvasElement>;
@@ -21,23 +20,37 @@ export class AppComponent implements OnInit {
   private ctx: CanvasRenderingContext2D;
   private graph = new Graph<Room, number>();
 
-  // VERTEX(ID, DATA)
-  // EDGE(FROM, TO, WEIGHT)
+  private img = new Image();
 
+  private firstRun = true;
   ngOnInit(): void {
-    // draw map
-    this.ctx = this.canvas.nativeElement.getContext('2d');
-    const img = new Image();
-    img.src = 'assets/SAHS_MAP_UP.png';
-    img.onload = () => {
-      this.ctx.scale(this.scale, this.scale);
-      this.ctx.drawImage(img, 0, 0);
-      this.ctx.strokeStyle = '#ff0000';
-      this.ctx.lineWidth += 3;
+    if (this.firstRun) {
+      // draw map
+      this.ctx = this.canvas.nativeElement.getContext('2d');
+      this.img.src = 'assets/SAHS_MAP_UP.png';
+      this.img.onload = () => {
+        this.ctx.drawImage(this.img, 0, 0);
+        this.ctx.strokeStyle = '#ff0000';
+        this.ctx.lineWidth = 3;
+
+        //shortest path stuff
+        // const path = new ShortestPath(this.graph);
+        // console.log(path.shortestPath(this.roomToID('C11U'), this.roomToID('D24U')));
+
+        //draw all edges
+        for (let i = 0; i < this.graph.EdgeNumber(); i++) {
+          this.drawEdge(this.graph.Edges()[i]);
+        }
+      };
+
+      this.ctx.canvas.width = 2200;
+      this.ctx.canvas.height = 1500;
+      this.ctx.drawImage(this.img, 10, 10);
+      this.firstRun = false;
 
       // add all rooms as vertices
       ROOMS.forEach(room => this.graph.AddVertex(this.graph.Vertexs().length + 1, room));
-      // console.log(this.roomToID('H212'));
+      console.log('hello');
 
       // add all edges to graph
       EDGES.forEach(edge => {
@@ -51,17 +64,7 @@ export class AppComponent implements OnInit {
         }
       );
 
-      const path = new ShortestPath(this.graph);
-      console.log(path.shortestPath(this.roomToID('C11U'), this.roomToID('D24U')));
-
-      for (let i = 0; i < this.graph.EdgeNumber(); i++) {
-        this.drawEdge(this.graph.Edges()[i]);
-      }
-    };
-
-    this.ctx.canvas.width = 1400;
-    this.ctx.canvas.height = 1500;
-    this.ctx.drawImage(img, 10, 10);
+    }
 
   }
 
@@ -69,8 +72,9 @@ export class AppComponent implements OnInit {
     const room1: Room = this.graph.Vertexs().find(v => v.id === edge.to).value;
     const room2: Room = this.graph.Vertexs().find(v => v.id === edge.from).value;
 
-    this.ctx.moveTo(room1.xPosition * this.scale, room1.yPosition * this.scale);
-    this.ctx.lineTo(room2.xPosition * this.scale, room2.yPosition * this.scale);
+    this.ctx.beginPath();
+    this.ctx.moveTo(room1.xPosition, room1.yPosition);
+    this.ctx.lineTo(room2.xPosition, room2.yPosition);
     this.ctx.stroke();
   }
 
@@ -78,5 +82,18 @@ export class AppComponent implements OnInit {
     return this.graph.Vertexs().find(v => v.value.roomNumber === roomNum).id;
   }
 
-
+  private switchMapNum = 0;
+  switchMap(): void {
+    if (this.switchMapNum % 2 === 1) {
+      // this.ctx.canvas.width = 1400;
+      // this.ctx.canvas.height = 1500;
+      this.ctx.canvas.height = 1500;
+      this.img.src = 'assets/SAHS_MAP_UP.png';
+    } else {
+      this.ctx.canvas.height = 1650;
+      this.img.src = 'assets/SAHS_MAP.png';
+    }
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.switchMapNum++;
+  }
 }
